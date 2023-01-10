06fca164bca89e6530cffd83bc516d91b531100a
Daniel Ohrin <xqiam@outlook.com>
2023-01-09 23:15:17 -0600

    Halfway Done

--- src/com/daniel/guessinggame/Main.java
+++ src/com/daniel/guessinggame/Main.java
@@ -1,25 +1,55 @@
 package com.daniel.guessinggame;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Game guessingGame = new Game();
-		
-		final int[] RESULT = guessingGame.run();
-		
-		if (RESULT[2] == 1) {
-			System.out.println("An Error occured with the scanner. Restart the application.");
-			return;
+		ArrayList<Boolean> gameHistory = new ArrayList<Boolean>();
+		String isPlaying = "y";
+
+		// Open a scanner
+		Scanner scanner = new Scanner(System.in);
+
+		while (isPlaying.equalsIgnoreCase("Y")) {
+			Game guessingGame = new Game();
+
+			final int[] RESULT = guessingGame.run();
+
+			if (RESULT[2] == 1) {
+				scanner.close();
+				System.out.println("An Error occured with the scanner. Restart the application.");
+				return;
+			}
+
+			StringBuilder gameOverMsg = new StringBuilder("You ");
+			gameOverMsg.append(RESULT[0] == 1 ? "Win!" : "Lose.");
+			gameOverMsg.append(String.format("\nThe answer was %d", RESULT[1]));
+			gameOverMsg.append("\n\nPlay again? (Y/n): ");
+
+			Game.clearConsole();
+			System.out.println(gameOverMsg.toString());
+
+			// Always want this to run once, because the default value will be some form of
+			// "Y"
+			do {
+				try {
+					isPlaying = scanner.next();
+				} catch (Exception e) {
+					scanner.close();
+					System.out.println("An Error occured with the scanner. Restart the application.");
+					return;
+				}
+
+				if (isPlaying.equalsIgnoreCase("N")) {
+					break;
+				}
+			} while (!isPlaying.equalsIgnoreCase("Y") && !isPlaying.equalsIgnoreCase("N"));
 		}
-		
-		StringBuilder gameOverMsg = new StringBuilder("You ");
-		gameOverMsg.append(RESULT[0] == 1 ? "Win!" : "Lose.");
-		gameOverMsg.append(String.format("\nThe answer was %d", RESULT[1]));
-		
-		Game.clearConsole();
-		System.out.println(gameOverMsg.toString());
+
+		// Close the scanner
+		scanner.close();
 	}
 
 }
